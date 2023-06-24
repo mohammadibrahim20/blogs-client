fa1e6135471a3f84689268178e3fa842ebeb1907
mohammadibrahim20 <ibinfo2017bd@gmail.com>
2023-06-24 13:32:39 +0600

    Form Complete

--- src/Pages/AddBlog/AddBlog.jsx
+++ src/Pages/AddBlog/AddBlog.jsx
@@ -6,19 +6,23 @@ const KeyCodes = {
   comma: 188,
   enter: 13,
 };
-
 const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
 const AddBlog = () => {
   const [tags, setTags] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [category, setCategory] = useState("");
+  const handleFileChange = (event) => {
+    const selectedFiles = event.target.files;
+    setPhotos([...photos, ...selectedFiles]);
+  };
 
   const handleDelete = (i) => {
     setTags(tags.filter((tag, index) => index !== i));
   };
-
   const handleAddition = (tag) => {
     setTags([...tags, tag]);
   };
-
   const handleDrag = (tag, currPos, newPos) => {
     const newTags = tags.slice();
 
@@ -33,10 +37,19 @@ const AddBlog = () => {
     console.log("The tag at index " + index + " was clicked");
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const title = form.title.value;
+    const description = form.description.value;
+    const url = form.url.value;
+    console.log(title, description, url, category);
+  };
+  console.log(category);
   return (
     <div className="max-w-7xl mx-auto w-full mt-10 px-5">
       <h2 className="text-4xl font-bold text-blue-600">Add Portfolio</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="md:flex gap-5">
           <div className="form-control w-full">
             <label className="label">
@@ -44,6 +57,7 @@ const AddBlog = () => {
             </label>
             <input
               type="text"
+              name="title"
               placeholder="Type here"
               className="input input-bordered w-full "
             />
@@ -52,8 +66,11 @@ const AddBlog = () => {
             <label className="label">
               <span className="label-text font-bold">Technology Category</span>
             </label>
-            <select className="select select-bordered">
-              <option>Web Developer</option>
+            <select
+              onChange={(event) => setCategory(event.target.value)}
+              className="select select-bordered"
+            >
+              <option selected>Web Developer</option>
               <option>Graphic Designer</option>
               <option>Digital Marketing</option>
             </select>
@@ -66,6 +83,8 @@ const AddBlog = () => {
           <input
             type="file"
             className="file-input file-input-bordered w-full "
+            multiple
+            onChange={handleFileChange}
           />
         </div>
         <div className="form-control w-full mt-5">
@@ -93,6 +112,8 @@ const AddBlog = () => {
               type="url"
               placeholder="Type here"
               className="input input-bordered w-full "
+              name="url"
+              required
             />
           </div>
         </div>
@@ -103,10 +124,16 @@ const AddBlog = () => {
           <textarea
             className="textarea textarea-bordered h-24"
             placeholder="Description"
+            name="description"
+            required
           ></textarea>
         </div>
-        <div className="mt-5"> 
-          <input type="submit" value="Add Portfolio" className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"/>
+        <div className="mt-5">
+          <input
+            type="submit"
+            value="Add Portfolio"
+            className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+          />
         </div>
       </form>
     </div>
